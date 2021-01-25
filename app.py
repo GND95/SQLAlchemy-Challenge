2f119e30484270bd5216e92df2403a82b06f7a53
Garrett DeBlois <garrettdeblois13@yahoo.com>
2021-01-24 18:35:31 -0600

    performing initial SQLAlchemy setup in Flask app

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
+from flask import Flask, jsonify
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
 
-from flask import Flask
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", echo=False)
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 app = Flask(__name__)
 
